c56e96b09f42c00939b92033dca8dc770c357e8e
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-03-02 11:06:47 +0900

    footerの横幅をNavbarの横幅と揃える

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,34 +9,36 @@ const Layout = ({ location, title, children }) => {
   const isRootPath = location.pathname === rootPath
  
   return (
-    <Container data-is-root-path={isRootPath}>
-      <header className={styles.globalHeader}>
-        <Container>
-          <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Container>
-      </header>
-      <main>{children}</main>
-      <footer>
+    <>
+      <div className="global-wrapper" data-is-root-path={isRootPath}>
+        <header className={styles.globalHeader}>
+          <Container>
+            <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
+              <Navbar.Brand href="/">{title}</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="#deets">More deets</Nav.Link>
+                  <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </Container>
+        </header>
+        <main>{children}</main>
+      </div>
+      <footer className="text-center bg-dark text-white">
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
-    </Container>
+    </>
   )
 }
 
