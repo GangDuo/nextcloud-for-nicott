cd6731efb9b7577f2ce3b6678d74acb4ea02c1dc
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-03-02 11:50:05 +0900

    custom CSS stylesを適用できなかった

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./layout.module.css";
 
 const Layout = ({ location, title, children }) => {
