385dcf39fb6e599ebf108a58fa007b1b235d0935
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-03-02 10:10:56 +0900

    Navbar追加

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "./layout.module.css";
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+ 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <Container data-is-root-path={isRootPath}>
+      <header className={styles.globalHeader}>
+        <Container>
+          <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  Dank memes
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
-    </div>
+    </Container>
   )
 }
 
