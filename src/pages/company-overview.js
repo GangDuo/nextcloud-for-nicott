bb6499e9327f7a94dcb82bea3e6a0a573ec3c4a3
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-03-03 17:39:47 +0900

    会社概要ページ追加 #16

---
+++ src/pages/company-overview.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const CompanyOverviewPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="会社概要" />
+      <h1>会社概要</h1>
+    </Layout>
+  )
+}
+
+export default CompanyOverviewPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
